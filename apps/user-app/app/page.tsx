60e53911bb8eeca78974a8b4dd839382b8a4d0e0
Divyansh Nahar <divyanshnahar15@gmail.com>
2025-06-04 02:23:10 +0530

    followed some steps

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
-import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
- 
-
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient();
 export default function Page(): JSX.Element {
   return (
    <div className = 'text-center'>
