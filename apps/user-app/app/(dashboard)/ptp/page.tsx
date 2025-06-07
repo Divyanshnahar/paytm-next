ed3c7581f468a69adedd7e48de38673856f08b48
Divyansh Nahar <divyanshnahar15@gmail.com>
2025-06-07 17:30:48 +0530

    completed according to course

--- apps/user-app/app/(dashboard)/ptp/page.tsx
+++ apps/user-app/app/(dashboard)/ptp/page.tsx
@@ -1,7 +1,46 @@
+import { getServerSession } from "next-auth";
 import { SendCard } from "../../../components/sendCard" 
+import { authOptions } from "../../lib/auth";
+import { PtpTxnCard } from "../../../components/ptpTxnCard";
+import prisma from "@repo/db/client";
 
-export default function() {
-    return <div className="w-full">
-        <SendCard />
+export default async function() {
+    // const balance = await getBalance();
+    const transactions = await getptpTransaction();
+
+    return <div className="w-screen">
+        <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+            Transfer
+        </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+            <div>
+                <SendCard />
+            </div>
+            <div>
+                
+                <div className="pt-4">
+                    <PtpTxnCard transactions={transactions} />
+                </div>
+            </div>
+        </div>
     </div>
+}
+
+
+
+async function getptpTransaction() {
+    const session = await getServerSession(authOptions);
+    
+    const txns = await prisma.p2pTransfer.findMany({
+        where: {
+            fromUserId: Number(session?.user?.id)
+        }
+    });
+
+    return txns
+        .sort((a, b) => b.timestamp.getTime() - a.timestamp.getTime())
+        .map(t => ({
+            time: t.timestamp,
+            amount: t.amount,
+        }));
 }
